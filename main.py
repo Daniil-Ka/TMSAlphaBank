d413187cfead2a21e52361c275838b5887b75214
Daniil-Ka <dkrapivin.04@gmail.com>
2023-11-01 12:11:23 +0500

    uvicorn и рендер страницы

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi_htmx import htmx, htmx_init
+
+from cases import Cases
 
 app = FastAPI()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='static/templates')
+htmx_init(templates=templates)
 
 
 @app.get('/', response_class=HTMLResponse)
+@htmx("index", "index")
 async def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
 
 
 @app.post('/compute')
 async def compute(text=Form()):
-    return {'message': text}
+    return {
+        Cases.Nominative: 'text',
+        Cases.Genitive: 'text',
+        Cases.Dative: 'text',
+        Cases.Accusative: 'text',
+        Cases.Instrumental: 'text',
+        Cases.Prepositional: 'text'
+    }
