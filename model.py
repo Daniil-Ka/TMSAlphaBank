cb175f9ff93fcde00670b1a65a4807b33121cbee
Daniil-Ka <dkrapivin.04@gmail.com>
2023-12-08 14:01:41 +0500

    примеры судов и 2 модель

--- model.py
+++ model.py
@@ -17,7 +17,8 @@ class Model:
         self.vectorizer = TextVectorization.from_config(cfg['config'])
         self.vectorizer.set_weights(cfg['weights'])
 
-        self.model = tf.keras.models.load_model('my_model.h5')
+        self.model = tf.keras.models.load_model('my_model.keras')
+        pass
 
 
     def translate_sentence(self, sentence):
@@ -30,22 +31,3 @@ class Model:
             [self.vectorizer.get_vocabulary()[i] for i in np.argmax(predicted_sequence, axis=2)[0]]
         )
         return predicted_sentence
-
-
-if __name__ == "__main__":
-    m = Model()
-    m.load()
-    sentences = [
-        ["Вагайский городской суд города нижний новгород", "Вагайским городским судом города нижний новгород"],
-        ["Бутурлинский межрайонный суд г. екатеринбурга свердловской области","Бутурлинским межрайонным судом г. екатеринбурга свердловской области"],
-        ["Армавирский городской суд г.новая ляля","Армавирским городским судом г.новая ляля"],
-        ["Азнакаевский городской суд республики саха","Азнакаевским городским судом республики саха"],
-        ["Беловский межрайонный суд волгоградской области","Беловским межрайонным судом волгоградской области"]
-    ]
-
-    for sent in sentences:
-        translated = m.translate_sentence(sent[0])
-        print("Оригинал: ", sent[0])
-        print("Требуемое: ", sent[1])
-        print("Перевод: ", translated)
-        print()
